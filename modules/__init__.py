96e688e27274bf1fd02f2b052a2dcea3b0bcbc20
Parth Shah <parthshah363363@gmail.com>
2025-06-23 12:01:24 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4YrzfpQ4kGXjuP6w'
+app.config['SECRET_KEY'] = '(SECRET KEY)'
 
 # Replace the below with your actual MySQL credentials and database name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/databasename'
